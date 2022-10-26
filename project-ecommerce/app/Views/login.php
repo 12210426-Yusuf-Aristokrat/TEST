ee67a0c841639b8234889027f0c06eba5761a39c
Yusuf-Aristokrat <12210426@bsi.ac.id>
2022-10-26 16:07:15 +0700

    tambah view login,pengguna/table

--- project-ecommerce/app/Views/login.php
+++ project-ecommerce/app/Views/login.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <title>Login Sistem Arsip Digital</title>
+ <title>Login | E-Commerce</title>
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ <!-- Costume : -->
+ <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+ <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+ <script src="jquery-3.6.0.min.js"></script>
+ <!-- --------- -->
  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/gh/agoenxz2186/submitAjax@develop/submit_ajax.js"></script>
+<!-- ---------- -->
  <meta charset="UTF-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -31,6 +38,5 @@
   </div>
  </form>
  </div>
- 
 </body>
 </html>
